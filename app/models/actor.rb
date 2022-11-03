6c5db5f8a1e2d02caad2039c7f58e5a90bc68b63
Evans Ochieng <evansochieng121@gmail.com>
2022-11-03 23:32:13 +0300

    practice many-to-many relationship in ar

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    #full name method
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    #list roles method
+    def list_roles
+        ["#{self.characters.first.name} - #{self.shows.first.name}"]
+    end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,8 @@
 class Network < ActiveRecord::Base
-  
+    has_many :shows
+
+    #sorry method
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
 end
